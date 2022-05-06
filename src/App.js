1f2fad70001059448e28ee08a8b6fa44e72e5e0d
Ayon <ayonjodder177@gmail.com>
2022-05-06 22:08:51 +0600

    Footer added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Components/Shared/Navbar/Navbar';
 import UpdateProduct from './Components/UpdateProduct/UpdateProduct';
 import Inventory from './Components/Inventory/Inventory'
 import AddNewItem from './Components/AddNewItem/AddNewItem'
+import Footer from './Components/Shared/Footer/Footer';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path='/add-item' element={<AddNewItem></AddNewItem>}></Route>
         <Route path='/inventory/:id' element={<UpdateProduct></UpdateProduct>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
